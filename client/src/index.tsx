8716ed6f4652dd6c16f6366cb3c9b5b8045f1871
Nguyen Thanh Tung <tung.nguyen@hdwebsoft.co>
2020-05-14 18:25:18 +0700

    Build app 3 update code company

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from 'react-redux'
 import App from './modules/App';
 import * as serviceWorker from './serviceWorker';
 import configureStore from './store';
 const store = configureStore();
-import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
     <Provider store={ store }>
--- client/src/layout/Authenticated.tsx
+++ client/src/layout/Authenticated.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import Loading from '../modules/Loading/container/Loading';
+import Header from '../components/layout/Header';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import LeftSidebar from '../components/layout/LeftSidebar'
 
 const Authenticated: React.FC<{}> = props => {
     return (
         <div className="authenticated">
+            <Header />
+            <div className="mainPage">
+                <Container>
+                    <Row>
+                        <Col lg={3} className="pageSidebar">
+                            <LeftSidebar />
+                        </Col>
+                        <Col lg={6} className="pageContent">
+                        </Col>
+                        <Col lg={3} className="pageTags">
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
             <Loading />
             { props.children }
         </div>
